970c02c426f23a3c41c50aaf98783eb4eebbe67b
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2023-09-09 10:26:42 -0400

    first commit

---
+++ Lexical-Asignacion.py
@@ -0,0 +1,13 @@
+# import ply lexer
+from ply import lex
+
+# Declaration of the assigned tokens
+# for lexer from ply to use when scanning the code
+assigned_tokens = [
+    'VAR',
+    'INT',
+    'DOUBLE',
+    'COMMENT',
+    'WHITESPACE'
+]
+
